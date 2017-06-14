6800ce110dd59ca39ba180129c2ac4276cbbe578
gavin myers <gavin@gavinm.com>
2017-06-13 20:15:53 -0500

    adding ref bash scripts

--- hs-app/haskell-webservice/build-run.sh
+++
@@ -1,4 +0,0 @@
-docker build . -f Dockerfile -t haskell-webservice-builder
-docker run --rm -v $(PWD)/.stack-work/dist/linux/bin:/root/.local/bin --name haskell-webservice-builder haskell-webservice-builder
-docker build . -f DockerfileRuntime -t haskell-webservice
-docker run --rm -p 3000:3000 --name haskell-webservice haskell-webservice
