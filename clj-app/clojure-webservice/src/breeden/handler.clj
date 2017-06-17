5e88545a8ac44da95c4990ecf81970244350a96c
gavin myers <gavin@gavinm.com>
2017-06-17 07:25:40 -0500

    got somethign up there... needs to be cleaned up

--- clj-app/clojure-webservice/src/breeden/handler.clj
+++ clj-app/clojure-webservice/src/breeden/handler.clj
@@ -4,11 +4,22 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [cheshire.core :refer :all] ))
 
+(defn numbers [coll] 
+  (filter number? coll))
+
+(defn avg [coll]
+  (/ (reduce + (numbers coll)) (max (count (numbers coll)) 1)))
+
+(defn get-speed [nodes] 
+  (avg (map read-string (map #(get % :SPD) nodes))))
+
+(defn parse-body [body] 
+  (map #(get-speed (val %)) (parse-string (slurp body) true)))
+
 (defroutes app-routes
   (GET "/" [] "Hello Gavin, this is the clojure webservice")
-  (POST "/" {body :body} (str  (keys (parse-string (slurp body) true))))
+  (POST "/" {body :body} (pr-str (parse-body body)))
   (route/not-found "Not Found"))
-
 
 (def app (wrap-defaults app-routes (assoc-in site-defaults [:security :anti-forgery] false)))
 
