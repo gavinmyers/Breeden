016be9e6a78f5a04defe241cd889fb2e35a4af33
gavin myers <gavin@gavinm.com>
2017-06-17 10:58:21 -0500

    getting haskell to read in json data

--- data/post.sh
+++ data/post.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST -H "Content-Type: application/json" -d @data.json http://localhost:3000 
+curl -X POST -H "Content-Type: application/json" -d @data-user.json http://localhost:3000 
--- hs-app/haskell-webservice/Main.hs
+++ hs-app/haskell-webservice/Main.hs
@@ -1,14 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 
 import Web.Scotty
 import Network.Wai.Middleware.RequestLogger
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Monoid ((<>))
+import GHC.Generics
+
+data User = User { userId :: Int, userName :: String } deriving (Show, Generic)
+instance ToJSON User
+instance FromJSON User
+obj :: User
+obj = User { userId = 1, userName = "bob" }
 
 main :: IO ()
 main = scotty 3000 $ do
   middleware logStdoutDev
+  post "/" $ do
+    resp <- jsonData 
+    json (resp :: User) 
+
+  get "/health" $ do
+    text "UP"
+
   get "/:word" $ do
     beam <- param "word"
     html $ mconcat ["<h1>Gavin, ", beam, " me up!</h1>"]
 
-  get "/health" $ do
-    text "UP"
